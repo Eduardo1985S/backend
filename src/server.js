6c819e4160ce01c9bfef5a30508b77a1c5594162
Eduardo Correia <dev.edu@hotmail.com>
2026-09-01 12:02:39 -0300

    feat: initialize Express server and update environment configuration variables

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import produtoRoutes from './routes/produtoRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middlewares
 app.use(cors());
